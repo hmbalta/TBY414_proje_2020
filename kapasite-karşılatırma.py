f3690c571c39dbc7fc286a0c6af32d9c4d918a6c
borareyhan <61205752+borareyhan@users.noreply.github.com>
2020-06-23 00:35:26 +0300

    Update kapasite-karşılatırma.py

--- kapasite-karşılatırma.py
+++ kapasite-karşılatırma.py
@@ -1,11 +1,15 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
+## BU KODDA SABİHA GÖKÇEN VE ATATRÜK HAVAALANLARININ 2018 YILINA KADAR OLAN VERİLERİ VE 2018 YILINDAN SONRA
+## TAHMİNLERLE YOLA ÇIKILARAK BULUNMUŞ KAPASİTE SAYILARI KARŞILAŞTIRILMIŞTIR
 
 df = pd.read_csv('veriler.csv')
 print(df)
 
 ucusAtaturk = df.loc[:,'Atatürk']
-kapasiteAtaturk = df.loc[:, 'Atatürk Kapasite']
-tarihlerAtaturk = df.loc[:, 'Unnamed: 0']
+kapasiteAtaturk = df.loc[:, 'AtatürkKapasite']
+tarihlerAtaturk = df.loc[:, 'Tarih']
 
 diziAtaturk = []
 sayac = 0
@@ -13,13 +17,13 @@ for i in range(len(df)):
     if ucusAtaturk[i] - kapasiteAtaturk[i]>0:
         diziAtaturk.append(tarihlerAtaturk[i])
         i = i + 1
-print('Atatürk Havalimanında Kapasite Aşılan Aylar')
+print('Atatürk Havalimanında Kapasite Aşılan Aylar(2019 Yılı Sonrası Tahmin edilerek yazılmıştır)')
 print(diziAtaturk)
 print('Atatürk Havalimanında Kapasite Aşılan Aylar Toplamı:' , len(diziAtaturk))
 
-ucusSabiha = df.loc[:,'Sabiha Gökçen']
-kapasiteSabiha = df.loc[:, 'Sabiha Kapasite']
-tarihlerSabiha = df.loc[:, 'Unnamed: 0']
+ucusSabiha = df.loc[:,'SabihaGökçen']
+kapasiteSabiha = df.loc[:, 'SabihaKapasite']
+tarihlerSabiha = df.loc[:, 'Tarih']
 
 diziSabiha = []
 sayac = 0
@@ -27,8 +31,29 @@ for i in range(len(df)):
     if ucusSabiha[i] - kapasiteSabiha[i]>0:
         diziSabiha.append(tarihlerSabiha[i])
         i = i + 1
-print('Sabiha Gökçen Havalimanında Kapasite Aşılan Aylar')
+print('Sabiha Gökçen Havalimanında Kapasite Aşılan Aylar(2019 Yılı Sonrası Tahmin edilerek yazılmıştır)')
 print(diziSabiha)
 print('Sabiha Gökçen Havalimanında Kapasite Aşılan Aylar Toplamı:' , len(diziSabiha))
+
+plt.figure(figsize=(12,10))
+plt.subplot(2,2,1)  
+plt.title("Atatürk Havaalanı Kapasitesi Karşılaştırılması")
+plt.plot(df.Tarih,df.Atatürk,color="blue")
+plt.plot(df.AtatürkKapasite,color="red")
+plt.xlabel("Tarih")
+plt.ylabel("Yolcu Sayıları")
+
+plt.subplot(2,2,2)
+plt.title("Sabiha Gökçen Havaalanı Kapasitesi Karşılaştırılması")
+plt.plot(df.Tarih,df.SabihaGökçen,color="blue")
+plt.plot(df.SabihaKapasite,color="red")
+plt.xlabel("Tarih")
+plt.ylabel("Yolcu Sayıları")
+
+plt.show()
+
+
+
+
 
 
